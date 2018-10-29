43eef7f41efc6cb383a87bc7a55df4f2996dd4e3
Paul Holtz <paul.holtz@qvc.com>
2018-10-29 18:29:25 -0400

    bugfix to actually set the enhanced element back into the map

--- src/main/java/com/scottstots/task/TaskController.java
+++ src/main/java/com/scottstots/task/TaskController.java
@@ -1,12 +1,10 @@
 package com.scottstots.task;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.task.TaskQuery;
@@ -19,16 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController
 {
 	private static final Logger log = LoggerFactory.getLogger(TaskController.class);
+	private final ProcessEngine processEngine;
 	
-	@GetMapping("/rest/v1/us/task-hierarchy")
+	public TaskController(ProcessEngine processEngine) {
+		this.processEngine = processEngine;
+	}
+	
+	@GetMapping("/v1/us/task-hierarchy")
 	public TaskElement retrieveTasksWithHierarchy() {
-		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 		TaskService taskService = processEngine.getTaskService();
 		TaskQuery taskQuery = taskService.createTaskQuery().active();
 		List<Task> tasks = taskQuery.list();
 		
 		Map<String, TaskElement> aggregatedTaskElements = new HashMap<>();
 		for(Task task : tasks) {
+			log.info("Iterating over task with id {} and parent task id {}", task.getId(), task.getParentTaskId());
 			// Retrieve the partially aggregated task element using the "id" field of the bpmn
 			TaskElement taskElement = aggregatedTaskElements.get(task.getTaskDefinitionKey());
 			if(taskElement == null) {
@@ -39,8 +42,8 @@ public class TaskController
 			}
 			int elementCount = Integer.valueOf(taskElement.getDesignation()).intValue();
 			taskElement.setDesignation(String.valueOf(elementCount + 1));
+			aggregatedTaskElements.put(task.getTaskDefinitionKey(), taskElement);
 		}
-		
 		
 		return null;
 	}
